72c686e6e376325f292b41e0b4b791f24ceec979
FUJISE Takumi <fujise@lean-system.co.jp>
2015-08-31 13:44:24 +0900

    rjs test

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,23 @@
 class PagesController < ApplicationController
+  before_action :set_page, only: [:update]
+
   def index
+    @pages = Page.all
+  end
+
+  def update
+    # @page.update(page_params)
+    @pages = Page.all
+    @page.update(page_params)
+    @page.save
+  end
+
+private
+  def set_page
+    @page = Page.find(params[:id])
+  end
+
+  def page_params
+    params.require(:page).permit(:title, :name)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :pages, only: :index
+  resources :pages#, only: :index
 
   # The priority is based upon order of creation: first created ->
   # highest priority.
